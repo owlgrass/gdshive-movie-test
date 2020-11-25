84b9d6eb3cf9665b0d96897b86b1cf8dba7b5e3d
owlgrass <tsf.works@gmail.com>
2020-11-25 17:16:50 +0800

    Tweak visuals for vSelect

--- src/views/Home/vSelect.vue
+++ src/views/Home/vSelect.vue
@@ -6,6 +6,7 @@
 	<select 
 		:value="value" 
 		@input="$emit('input', $event.target.value)"
+		class="v-select"
 	>
 		<option 
 			v-if="undefinedOptionName"
@@ -44,5 +45,36 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+.v-select {
+	appearance: none;
+	-moz-appearance: none;
+	-webkit-appearance: none;
+	background: transparent;
+	padding: 0.3em 1.2em 0.3em 0em;
+
+	border: 1px solid transparent;
+	color: var(--accent-color);
+	outline: none;
+
+	/* Draw an arrow on the right*/
+	background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath d='M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z' fill='%23CCC'/%3E%3Cpath d='M0 0h24v24H0z' fill='none'/%3E%3C/svg%3E");
+	background-repeat: no-repeat;
+	background-position: center right;
+	background-size: 1.2em;
+}
+.v-select:hover {
+	color: var(--accent-color-active);
+	border: 1px solid var(--accent-color-active);
+}
+.v-select:focus {
+	color: var(--accent-color-active);
+}
+
+.v-select option {
+}
+.v-select option:disabled {
+	color: var(--main-color-disabled);
+}
 </style>
