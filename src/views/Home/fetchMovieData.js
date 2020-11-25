dd68119373cf1ae81b3ec60b9e394eb1c7ef3f70
owlgrass <tsf.works@gmail.com>
2020-11-25 15:25:53 +0800

    Add more error messages for fetch fails

--- src/views/Home/fetchMovieData.js
+++ src/views/Home/fetchMovieData.js
@@ -4,15 +4,20 @@ const fetchMovieData = async function() {
 	const request = 'https://sometimes-maybe-flaky-api.gdshive.io/'
 
 	const response = await fetch(request)
+	if (!response.ok) {
+		throw `API server request failed with ${response.status}`
+	}
+
 	const movies = await response.json()
 
-	// Ensure movies data is Array and has name property
+	// Ensure movies is Array and has name property
 	if (
 		!Array.isArray(movies) ||
 		!movies.every(m => m.name)
 	) {
 		throw 'Data invalid'
 	}
+
 
 	return movies
 }
