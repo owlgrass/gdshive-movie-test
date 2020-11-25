e4d421865959de67a49f4305c0fe1f40fa6da64a
owlgrass <tsf.works@gmail.com>
2020-11-25 15:36:42 +0800

    Refactor movie fetching handling code from MovieGrid to home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,20 +5,50 @@
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 			</p>
-		</header>	
-		<MovieGrid />
+		</header>
+
+
+		<div v-if="error" class="error">
+			<b>Error:</b> {{ this.error }}
+		</div>
+		<div v-else-if="loading" class="loading">
+			Fetching movies...
+		</div>
+		<MovieGrid v-else :movies="movies" />
 	</div>
 </template>
 
 <script>
 import MovieGrid from './Home/MovieGrid'
+import fetchMovieData from './Home/fetchMovieData'
 
 export default {
 	name: 'Home',
 	data() {
 		return {
-			
+			loading: false,
+			error: false,
+			movies: []
 		}
+	},
+	methods: {
+		fetchData() {
+			this.loading = true
+
+			fetchMovieData()
+				.then(movies => this.movies = movies)
+				.catch(error => {
+					this.error = error
+				})
+				.finally(() => this.loading = false)
+
+			// import('@/assets/test-data.json').then(data => {
+			// 	this.movies = data
+			// })
+		}
+	},
+	created() {
+		this.fetchData()
 	},
 	components: {
 		MovieGrid
@@ -30,4 +60,20 @@ export default {
 .home > header {
 	padding: 1em;
 }
+
+.home > .loading {
+	padding: 1em;
+	animation: 500ms ease-in-out loading alternate infinite;
+}
+
+@keyframes loading {
+	from { opacity: 100%; }
+	to { opacity: 30%; }
+}
+
+.home > .error {
+	padding: 1em;
+	color: var(--danger-color);
+}
+
 </style>
--- src/views/Home/MovieGrid.vue
+++ src/views/Home/MovieGrid.vue
@@ -1,16 +1,7 @@
 <template>
 	<div class="movie-grid">
 
-		<div v-if="error" class="error">
-			<b>Error:</b> {{ this.error }}
-		</div>
-
-		<div v-else-if="loading" class="loading">
-			Fetching movies...
-		</div>
-
 		<MovieGridItem 
-			v-else
 			class="grid-item"
 			v-for="movie in movies" 
 			:movie="movie" 
@@ -22,35 +13,18 @@
 
 <script>
 import MovieGridItem from './MovieGridItem'
-import fetchMovieData from './fetchMovieData'
 
 export default {
 	name: 'MovieGrid',
+	props: {
+		movies: {
+			type: Array,
+			required: true
+		}
+	},
 	data() {
 		return {
-			loading: false,
-			error: false,
-			movies: []
 		}
-	},
-	methods: {
-		fetchData() {
-			this.loading = true
-
-			fetchMovieData()
-				.then(movies => this.movies = movies)
-				.catch(error => {
-					this.error = error
-				})
-				.finally(() => this.loading = false)
-
-			// import('@/assets/test-data.json').then(data => {
-			// 	this.movies = data
-			// })
-		}
-	},
-	created() {
-		this.fetchData()
 	},
 	components: {
 		MovieGridItem
@@ -65,18 +39,4 @@ export default {
 	flex-wrap: wrap;
 }
 
-.movie-grid > .loading {
-	padding: 1em;
-	animation: 500ms ease-in-out movie-grid-loading alternate infinite;
-}
-
-@keyframes movie-grid-loading {
-	from { opacity: 100%; }
-	to { opacity: 30%; }
-}
-
-.movie-grid > .error {
-	padding: 1em;
-	color: var(--danger-color);
-}
 </style>
